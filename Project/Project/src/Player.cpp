f9a625d56fc30c9f9bf3bcec42a8eb9cb53d7187
raresgherasa <145050817+raresgherasa@users.noreply.github.com>
2025-06-06 10:50:12 +0300

    Delete Project directory

--- Project/Project/src/Player.cpp
+++
@@ -1,6 +0,0 @@
-#include "Player.h"
-
-Player::Player(const std::string& n, float s) : name(n), score(s) {}
-
-std::string Player::getName() const { return name; }
-float Player::getScore() const { return score; }
